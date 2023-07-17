2c18a0786a361dfafd974e9848d0471a4304f9e7
Farneiy Gomes Jr <104699938+jukafarneiy@users.noreply.github.com>
2023-07-17 15:37:11 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Diárias"),
+      ),
+      body: const Center(
+        child: Text("Versão Inicial"),
+      ),
+    );
+  }
+}
